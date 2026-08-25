3f36e3523e74f2d9c794e531580a58f1a2e085a3
ralphstevencimanes-bit <ralphstevencimanes@gmail.com>
2026-08-25 12:50:40 +0800

    Add files via upload
    08/25/26

---
+++ Activity5.py
@@ -0,0 +1,35 @@
+#arithmetic operators = mathematical symbols 
+# + - addition 
+# - - subtraction
+# * - multiplication 
+# / - division 
+# // - floor division
+# ** - exponentation 
+# % - modulus
+
+
+a = 10 
+b = 25 
+sum = a + b
+diff = a - b
+
+print("The sum of ",a,"plus ",b,"is ",sum)
+print("The sum of ",a,"minus",b,"is ",diff)
+print("The sum of ",a,"times ",b,"is ",a*b)
+print("The sum of ",a,"division ",b,"is ",a/b)
+print("The sum of ",a,"floor division ",b,"is ",a//b)
+print("The sum of ",a,"exponentation ",b,"is ",a**b)
+print("The sum of ",a,"modulus ",b,"is ",a%b)
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Code_Challenge2.py
@@ -0,0 +1,25 @@
+#Code_Challenge2
+
+money = 6769
+First = 1000 
+Second = 500
+Third = 200
+Fourth = 100
+Fifth = 50
+Six = 20
+Seven = 10
+Eight = 5
+Nine = 1
+print("money to deposit ---->",money)
+print("1000 =", money//First)
+print("500 =", money// Second)
+print("200 =", money//Third)
+print("100 =", money//Fourth)
+print("50 =", money//Fifth)
+print("20 =", money//Six)
+print("10 =", money//Seven)
+print("5 =", money//Eight)
+print("1 =", money//Nine)
+
+
+
