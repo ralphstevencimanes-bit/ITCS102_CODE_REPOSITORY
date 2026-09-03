d8813808827ff60d19d3c96b2c72661719365c08
ralphstevencimanes-bit <ralphstevencimanes@gmail.com>
2026-09-03 12:32:23 +0800

    Add files via upload
    sports

---
+++ Activity8.py
@@ -0,0 +1,16 @@
+#concatenation = combining string
+
+sports = "" #blank string 
+
+s = input("What are your sport --> ")
+sports += s + ", "
+
+s = input("Wow Galing mo naman, What else --> ")
+sports += s + ", "
+
+s = input("Nice one, What else --> ")
+sport += s + ", "
+
+
+print("So, my sports are ", sports)
+
