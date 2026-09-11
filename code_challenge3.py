fbb48b40144ef42381a7ac175641ef8c90bde56d
ralphstevencimanes-bit <ralphstevencimanes@gmail.com>
2026-09-11 16:00:01 +0800

    code_challenge3.py

--- code_challenge3.py
+++ code_challenge3.py
@@ -1,39 +1,61 @@
 #Global Freight Calculator
 
-Sender_name = input("Sender's thy name -----> ")
-Type_of_item = input("What is thy item being shipped? ----> ")
-is_Fragile = bool(input("Is thy Item fragile? -----> "))
-weight = float(input("What is thy weight of thy item -----> "))
-distance = float(input("What is thy distance of thy home? ------> "))
-is_express = bool(input("Is thy order a express order? ------> "))
-is_international = bool(input("Is thy order an international order? ------> "))
+name = input("Sender's thy name -----> ")
+item = input("What is thy item being shipped? ----> ")
 
-print("Thy name is", Sender_name)
-print("Thy item being shipped is", Type_of_item)
-
-if is_Fragile == 'True':
-	print("We will notify thy carrier.")
+Fragrile_input = input("Is thy Item fragile?  (yes/no) -----> ")
+if Fragrile_input =="yes":
+	 is_Fragrile = true
 else:
-	print("Thank the for answering.")
+	is_Fragrile = false
+	
+weight = float(input("how heavy is it in kg?  -----> "))
+distance = float(input("how far destinaton in km? ------> "))
 
-print("The weight of the item is", weight,"kg")
-print("The distance of the place is", distance,"km")
-
-base_cost = (weight * 2.50) + (distance * 0.15)
-Total = 0.00
-Total1 = (base_cost * 1.40) + 50
-Total2 = (base_cost * 1.20) + 25
-Total3 = (base_cost + 30)
-Total4 = (base_cost)
-
-if weight <= 2.0 and distance <= 100 and is_international == 'False' and is_express == 'False':
-	print("The cost of the shipment is $", Total)
-elif is_express == 'True' and is_international == 'True':
-	print("The cost of the shipment is $", Total1)
-elif is_express == 'True' or is_international == 'True' and weight > 20:
-	print("The cost of your shipment is $", Total2)
-elif  weight > 30 or distance > 1000:
-	pirnt("The cost of your shipment is $", Total3)
+express_input = input("is it in hurry (express)? (yes/no) ------> ")
+if express_input == True
+     is_express = True
 else:
-	print("The cost of your shipment is $", Total4)
+     is_express = False
+	
+international_input = input("Is it international? (yes/no) ------> ")
+if international_input == "yes"
+     is_international = True
+else:
+     is_international = False
+	
+base_cost=(weight * 2.50) + (distance * 0.15)
+
+if (
+	distance <= 100
+	and weight <= 2
+	and not is_express
+	and not is_international
+):
+	total = 0
+elif is_international and is_express:
+	total = (base_cost * 1.4) + 50
+elif weight > 20 and (is_international or is_express):
+	total = (base_cost * 1.2) + 25
+elif distance > 1000 or weight > 30:
+	total = base_cost + 30
+else:
+	total = base_cost
+
+if total == 0:
+	shipping_fee = 0:
+else:
+	shipping_fee = total - base_cost
+
+print("Sender:", name)
+print("Order:", item)
+print("Fragrile:", is_Fragrile)
+print("Weight:", weight, "kg")
+print("Distance:", distance, "km")
+print("Express:", is_express)
+print("International:", is_international)
+print("Base Cost: PHP", shipping_fee)
+print("Shipping Fee: PHP", shipping_fee)
+print("Total: PHP", total)
+
 
